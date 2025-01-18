80298140a66b1d66ffa9f06bc92cbf72d5ae8802
NtReadVirtualMemory <83749737+NtReadVirtualMemory@users.noreply.github.com>
2025-01-18 21:11:43 +0100

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -38,7 +38,6 @@ namespace offsets {
     inline constexpr uintptr_t InputObject = 0xF8;
     inline constexpr uintptr_t Inset = 0x310;
     inline constexpr uintptr_t JobId = 0x110;
-    inline constexpr uintptr_t TaskSchedulerPointer = 0x5C19D28;
     inline constexpr uintptr_t JumpPower = 0x1A8;
     inline constexpr uintptr_t LocalPlayer = 0x118;
     inline constexpr uintptr_t LocalScriptByteCode = 0x1C0;
@@ -46,7 +45,7 @@ namespace offsets {
     inline constexpr uintptr_t MaterialType = 0x2F0;
     inline constexpr uintptr_t MaxHealth = 0x1AC;
     inline constexpr uintptr_t MaxSlopeAngle = 0x1B0;
-    inline constexpr uintptr_t MeshPartColor3 = 0x0;
+    inline constexpr uintptr_t MeshPartColor3 = 0x190;
     inline constexpr uintptr_t ModelInstance = 0x2A0;
     inline constexpr uintptr_t ModuleScriptByteCode = 0x168;
     inline constexpr uintptr_t ModuleScriptBytecodePointer = 0x10;
@@ -55,11 +54,12 @@ namespace offsets {
     inline constexpr uintptr_t MoveDirection = 0x150;
     inline constexpr uintptr_t Name = 0x68;
     inline constexpr uintptr_t NameDisplayDistance = 0x274;
-    inline constexpr uintptr_t NameSize = 0x10;
+    inline constexpr uintptr_t NameSize = 0x249;
     inline constexpr uintptr_t Parent = 0x50;
     inline constexpr uintptr_t PartSize = 0x2B0;
     inline constexpr uintptr_t Ping = 0xC0;
     inline constexpr uintptr_t PlaceId = 0x170;
+    inline constexpr uintptr_t PlayerConfigurerPointer = 0x5B0A0A8;
     inline constexpr uintptr_t Position = 0x140;
     inline constexpr uintptr_t Primitive = 0x160;
     inline constexpr uintptr_t PrimitiveGravity = 0x114;
@@ -83,8 +83,9 @@ namespace offsets {
     inline constexpr uintptr_t SkyboxUp = 0x1C0;
     inline constexpr uintptr_t SoundId = 0xD0;
     inline constexpr uintptr_t StarCount = 0x20C;
-    inline constexpr uintptr_t StringSize = 0x10;
+    inline constexpr uintptr_t StringSize = 0x249;
     inline constexpr uintptr_t SunTextureId = 0x1E8;
+    inline constexpr uintptr_t TaskSchedulerPointer = 0x5C19D28;
     inline constexpr uintptr_t Team = 0x1D8;
     inline constexpr uintptr_t TeamColor = 0xC8;
     inline constexpr uintptr_t Transparency = 0xE8;
@@ -93,8 +94,12 @@ namespace offsets {
     inline constexpr uintptr_t Velocity = 0x14C;
     inline constexpr uintptr_t ViewportSize = 0x2D0;
     inline constexpr uintptr_t VisualEngine = 0x10;
+    inline constexpr uintptr_t VisualEnginePointer = 0x599B0A0;
+    inline constexpr uintptr_t VisualEngineToDataModel1 = 0x968;
+    inline constexpr uintptr_t VisualEngineToDataModel2 = 0x1A8;
     inline constexpr uintptr_t WalkSpeed = 0x1C8;
     inline constexpr uintptr_t WalkSpeedCheck = 0x3A0;
     inline constexpr uintptr_t Workspace = 0x150;
     inline constexpr uintptr_t viewmatrix = 0x4D0;
 }
+
