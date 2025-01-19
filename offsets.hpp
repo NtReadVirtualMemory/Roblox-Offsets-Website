552f1d5a15df73739d165b58213869f4c492bebc
NtReadVirtualMemory <83749737+NtReadVirtualMemory@users.noreply.github.com>
2025-01-19 21:11:51 +0100

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -18,10 +18,15 @@ namespace offsets {
     inline constexpr uintptr_t ClassDescriptor = 0x18;
     inline constexpr uintptr_t ClickDetectorMaxActivationDistance = 0x108;
     inline constexpr uintptr_t CreatorId = 0x160;
+    inline constexpr uintptr_t DataModelDeleterPointer = 0x5B67DA0;
+    inline constexpr uintptr_t DataModelDeleterPointerToDataModel1 = 0x18;
+    inline constexpr uintptr_t DataModelDeleterPointerToDataModel2 = 0x1A8;
     inline constexpr uintptr_t DataModelToRenderView1 = 0x1A0;
     inline constexpr uintptr_t DataModelToRenderView2 = 0x8;
     inline constexpr uintptr_t DataModelToRenderView3 = 0x28;
     inline constexpr uintptr_t DecalTexture = 0x100;
+    inline constexpr uintptr_t Deleter = 0x10;
+    inline constexpr uintptr_t DeleterBack = 0x18;
     inline constexpr uintptr_t Dimensions = 0x740;
     inline constexpr uintptr_t DisplayName = 0x108;
     inline constexpr uintptr_t FOV = 0x140;
@@ -45,7 +50,7 @@ namespace offsets {
     inline constexpr uintptr_t MaterialType = 0x2F0;
     inline constexpr uintptr_t MaxHealth = 0x1AC;
     inline constexpr uintptr_t MaxSlopeAngle = 0x1B0;
-    inline constexpr uintptr_t MeshPartColor3 = 0x190;
+    inline constexpr uintptr_t MeshPartColor3 = 0x0;
     inline constexpr uintptr_t ModelInstance = 0x2A0;
     inline constexpr uintptr_t ModuleScriptByteCode = 0x168;
     inline constexpr uintptr_t ModuleScriptBytecodePointer = 0x10;
@@ -54,7 +59,7 @@ namespace offsets {
     inline constexpr uintptr_t MoveDirection = 0x150;
     inline constexpr uintptr_t Name = 0x68;
     inline constexpr uintptr_t NameDisplayDistance = 0x274;
-    inline constexpr uintptr_t NameSize = 0x249;
+    inline constexpr uintptr_t NameSize = 0x10;
     inline constexpr uintptr_t Parent = 0x50;
     inline constexpr uintptr_t PartSize = 0x2B0;
     inline constexpr uintptr_t Ping = 0xC0;
@@ -83,12 +88,9 @@ namespace offsets {
     inline constexpr uintptr_t SkyboxUp = 0x1C0;
     inline constexpr uintptr_t SoundId = 0xD0;
     inline constexpr uintptr_t StarCount = 0x20C;
-    inline constexpr uintptr_t StringSize = 0x249;
+    inline constexpr uintptr_t StringSize = 0x10;
     inline constexpr uintptr_t SunTextureId = 0x1E8;
     inline constexpr uintptr_t TaskSchedulerPointer = 0x5C19D28;
-    inline constexpr uintptr_t DataModelDeleterPointer = 0x5B67DA0;
-    inline constexpr uintptr_t DataModelDeleterPointerToDataModel1 = 0x18;
-    inline constexpr uintptr_t DataModelDeleterPointerToDataModel2 = 0x1A8;
     inline constexpr uintptr_t Team = 0x1D8;
     inline constexpr uintptr_t TeamColor = 0xC8;
     inline constexpr uintptr_t Transparency = 0xE8;
@@ -105,4 +107,3 @@ namespace offsets {
     inline constexpr uintptr_t Workspace = 0x150;
     inline constexpr uintptr_t viewmatrix = 0x4D0;
 }
-
