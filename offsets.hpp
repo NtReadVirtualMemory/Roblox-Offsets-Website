8c7d2e203403022ea3f1263ff57deb162e65f2ae
NtReadVirtualMemory <83749737+NtReadVirtualMemory@users.noreply.github.com>
2025-04-10 16:41:38 +0200

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -71,7 +71,7 @@ namespace offsets {
     inline constexpr uintptr_t JobId = 0x120;
     inline constexpr uintptr_t JobStart = 0x1D0;
     inline constexpr uintptr_t Job_Name = 0x18;
-    inline constexpr uintptr_t JobsPointer = 0x62595B0;
+    inline constexpr uintptr_t JobsPointer = 0x6310810;
     inline constexpr uintptr_t JumpPower = 0x1B8;
     inline constexpr uintptr_t LocalPlayer = 0x128;
     inline constexpr uintptr_t LocalScriptByteCode = 0x1C0;
