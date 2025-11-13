c0d142112f25b9c50cb3f8367adc917a0b4a010c
NtReadVirtualMemory <83749737+NtReadVirtualMemory@users.noreply.github.com>
2025-11-13 14:28:34 +0100

    Update DataModel_Using_VisualEnginePointer.cpp

--- Methods/DataModel_Using_VisualEnginePointer.cpp
+++ Methods/DataModel_Using_VisualEnginePointer.cpp
@@ -1,4 +1,4 @@
 uintptr_t VisualEngine = read<uintptr_t>(mem::BaseAddress + offsets::VisualEnginePointer);
 uintptr_t DataModelPointer = read<uintptr_t>(VisualEngine + offsets::VisualEngineToDataModel1);
-uintptr_t game = read<uintptr_t>(DataModelPointer + offsets::VisualEngineToDataModel2);
+uintptr_t DataModel = read<uintptr_t>(DataModelPointer + offsets::VisualEngineToDataModel2);
 // Wow this is so UD!
